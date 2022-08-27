f1a7b2e44be2401b7d79a972c1c89f8330181bf3
Pau March <paumrch94@gmail.com>
2022-08-27 11:28:09 +0200

    domainUpdate

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = {
   images: {
     domains: [
       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      'wp.4thofjune.com',
       '0.gravatar.com',
       '1.gravatar.com',
       '2.gravatar.com',
