11a0f4a5fe3d31359e64b3328341b33b43b67eb2
Pau March <paumrch94@gmail.com>
2022-08-28 16:53:24 +0200

    Sitemap

---
+++ components/hero-cats.tsx
@@ -0,0 +1,47 @@
+import Date from './date'
+import CoverImage from './cover-image'
+import Link from 'next/link'
+import Categories from './categories'
+
+export default function HeroCats ({categories}) {
+  return (
+    <section>
+  <div className="py-8 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div className="mx-auto lg:mb-16 mb-8">
+          <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight">Categorías</h2>
+          <p>En 4thofJune encontrarás contenido sobre....</p>
+      </div> 
+      <div className="flex gap-8">
+      <article className="rounded-md border-2 border-gray-100 bg-white p-14">
+              <div className="flex justify-between items-center mb-5 text-gray-500">
+                  <span className="text-sm">12 Artículos</span>
+              </div>
+              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {/* <Categories categories={categories} /> */}
+              </h2>
+              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
+              <div className="flex justify-between items-center">
+                  <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                      Explorar
+                      <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  </a>
+              </div>
+          </article>      
+          <article className="rounded-md border-2 border-gray-100 bg-white p-14">
+              <div className="flex justify-between items-center mb-5 text-gray-500">
+                  <span className="text-sm">14 days ago</span>
+              </div>
+              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Ideas</a></h2>
+              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
+              <div className="flex justify-between items-center">
+                  <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                      Explorar
+                      <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  </a>
+              </div>
+          </article>                  
+      </div>  
+  </div>
+    </section>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,23 @@
-import Head from 'next/head'
-import { GetStaticProps } from 'next'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import Subscribe from '../components/subscribe'
-import { getAllPostsForHome } from '../lib/api'
-import { NextSeo } from 'next-seo'
-import { title } from 'process'
-import PostPreview from '../components/post-preview'
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import Subscribe from "../components/subscribe";
+import { getAllPostsForHome } from "../lib/api";
+import { title } from "process";
+import PostPreview from "../components/post-preview";
 
 export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
+  const heroPost = edges[0]?.node;
+  const morePosts = edges.slice(1);
 
   return (
-
     <Layout preview={preview}>
       <Head>
         <title>4thofJune</title>
-
       </Head>
       <Container>
         <Intro />
@@ -38,14 +35,14 @@ export default function Index({ allPosts: { edges }, preview }) {
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
     </Layout>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
-  const allPosts = await getAllPostsForHome(preview)
+  const allPosts = await getAllPostsForHome(preview);
 
   return {
     props: { allPosts, preview },
     revalidate: 10,
-  }
-}
+  };
+};
