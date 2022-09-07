1a9cb9afed964748c46c60af690d2e21f7ca4637
Pau March <paumrch94@gmail.com>
2022-09-07 11:23:19 +0200

    withSitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.4thofjune.com',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+  
