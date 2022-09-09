29814243c17140f1a92d8c68c7c5cc229540b988
Pau March <paumrch94@gmail.com>
2022-09-09 10:10:02 +0200

    yaFuncionaPages

--- lib/api.ts
+++ lib/api.ts
@@ -210,3 +210,32 @@ export async function getPostAndMorePosts(slug, preview, previewData) {
 
   return data
 }
+
+// Nueva función para hacer fetch a las Categorías
+
+export async function getAllPagesWithSlugs() {
+  const data = await fetchAPI(`
+  {
+    pages(first: 10000) {
+      edges {
+        node {
+          slug
+        }
+      }
+    }
+  }
+  `);
+  return data?.pages;
+}
+
+export async function getPageBySlug(slug) {
+  const data = await fetchAPI(`
+  {
+    page(id: "${slug}", idType: URI) {
+      title
+      content
+    }
+  }
+  `);
+  return data?.page;
+}
---
+++ pages/[slug].tsx
@@ -0,0 +1,33 @@
+import { getAllPagesWithSlugs, getPageBySlug } from '../lib/api';
+import Head from 'next/head';
+
+function Page(page) {
+    return (
+      <div className='flex flex-col p-10'>
+        <Head>
+          <title>{page.title}</title>
+        </Head>
+        <div className='mb-5 text-4xl font-bold'>{page.title}</div>
+        <div
+          className='text-base text-grey-darker'
+          dangerouslySetInnerHTML={{ __html: page.content }}
+        ></div>
+      </div>
+    );
+  }
+  
+
+export async function getStaticPaths() {
+    const pagesWithSlugs = await getAllPagesWithSlugs();
+    return {
+      paths: pagesWithSlugs.edges.map(({ node }) => `/${node.slug}`) || [],
+      fallback: true,
+    };
+  }
+
+  export async function getStaticProps({ params }) {
+    const page = await getPageBySlug(params.slug);
+    return { props: page };
+  }
+
+export default Page;
