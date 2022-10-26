c104efd437f002f15a87034c5559140e354b1c46
Pau March <paumrch94@gmail.com>
2022-10-26 11:54:22 +0200

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   
   return ( <>
 
     <Component {...pageProps} />
+    <Analytics />
     </>)
 
     }
